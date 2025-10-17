a4814ecec8fd89a8829705f5fcfdcc04c65370aa
Nana Yaw Agyeman <nanayawagyeman1@gmail.com>
2025-10-17 18:44:36 +0000

    chore(settings): preparing backend for deployment

--- eden_backend/settings.py
+++ eden_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hbehxsszax-+qunnk(6t75e1_l_&d!##(i#et%npdx84hgr=qf'
+SECRET_KEY =config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -89,6 +91,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = config('DATABASE_URL')
+DATABASES["default"] = dj_database_url.parse(database_url)
+    
+
+
 
 
 # Password validation
